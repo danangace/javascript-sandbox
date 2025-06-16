9c049f05401edd3d78ee9c66ddfa16f77300f990
danangace <danangbahari89@gmail.com>
2025-06-16 21:06:47 +0700

    practice with box-sizing

--- main.js
+++ main.js
@@ -87,19 +87,19 @@ function doOperation () {
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/all
  */
 
-const fetchPromise1 = fetch(
-  "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
-);
-const fetchPromise2 = fetch(
-  "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/not-found",
-);
-const fetchPromise3 = fetch(
-  "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json",
-);
+// const fetchPromise1 = fetch(
+//   "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
+// );
+// const fetchPromise2 = fetch(
+//   "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/not-found",
+// );
+// const fetchPromise3 = fetch(
+//   "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json",
+// );
 
-Promise.all([fetchPromise1, fetchPromise2, fetchPromise3])
-  .then(responses => {
-    for (const response of responses) {
-      console.log(`${response.url}: ${response.status}`);
-    }
-  })
+// Promise.all([fetchPromise1, fetchPromise2, fetchPromise3])
+//   .then(responses => {
+//     for (const response of responses) {
+//       console.log(`${response.url}: ${response.status}`);
+//     }
+//   })
